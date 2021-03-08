7610b96e704469a5c2843f46597ee4e7039684d0
99003509 <62369661+99003509@users.noreply.github.com>
2021-03-08 09:17:25 +0530

    Update main.c

--- PatientDoctor/main.c
+++ PatientDoctor/main.c
@@ -1,219 +1,70 @@
-//source file for Patient monitoring system
-#include <unistd.h>
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <stdlib.h>
-#define SIGHUP 1
-#define SIGINT 2
-#define SIGQUIT 3
-
-pthread_mutex_t m1=PTHREAD_MUTEX_INITIALIZER;
-
-sem_t s1;
-sem_t s2;
-sem_t s3;
-sem_t s4;
 
 
-void sighup(); 
-void sigint(); 
-void sigquit(); 
-void Get_Report1();
-void Get_Report2();
-void Get_Report3();
-void Get_Report4();
-
-void Alert_Doctor() 
-{ 
-  int pid;
-  
-     //
-			if ((pid = fork()) < 0)
-				{ 
-				perror("fork"); 
-				exit(1); 
-				} 
-  
-			if (pid == 0)
-				{	                    //child
-				signal(SIGHUP, sighup); 
-				signal(SIGINT, sigint); 
-				signal(SIGQUIT, sigquit); 
-				for (;;); 
-				} 
-				  
-  
-			else 
-				{ 
-				printf("\nPARENT: sending SIGHUP\n\n");  
-				kill(pid, SIGHUP); 
-  
-				sleep(3); 
-				printf("\nPARENT: sending SIGINT\n\n"); 
-				kill(pid, SIGINT); 
-  
-				sleep(3);
-				printf("\nPARENT: sending SIGQUIT\n\n"); 
-				kill(pid, SIGQUIT); 
-				sleep(3); 
-				} 
-		}
-		
-
-   
-void sighup() 
-  
-{ 
-    signal(SIGHUP, sighup); //reset signal
-    printf("Doctor has been alerted\n"); 
-} 
-  
-void sigint() 
-  
-{ 
-    signal(SIGINT, sigint); //reset signal
-    printf("Doctor treating the patient \n"); 
-} 
-  
-void sigquit() 
-{ 
-    printf("Treatment complete\n"); 
-
-}  
-	
-
+int main(int argc, char **argv) {
  
-int CheckVitals( int Breathing_rate, int Heart_beat)
-{
-	printf("\nPatient Vital Details : Heart_beat - %d and Breathing_rate - %d\n", Heart_beat, Breathing_rate);
-	if(Breathing_rate > 12 && Breathing_rate<20 )
-		{
-			if (Heart_beat>60 && Heart_beat<100)
-				{
-					printf("\nPatient is normal\n");
-				}
-			else 
-				{
-					printf("\nHeart_beat Problem - Alert the doctor\n");
-					 Alert_Doctor();
-				}
-		}
-		else
-			{
-				printf("\nPatient is in a critical situation - Alert the doctor as fast as possible\n");
-				Alert_Doctor();	
-			}
-			
+	//TODO: Define set-up required
+	NumPatient=10;
+	NumDR=3;
+	WRSize=4;											// Initialize the variables until receive their value
+    printf("#doctors %d, #patients %d, #chairs %d \n", NumDR, NumPatient, WRSize);
+ 
+	if(argc != 4){														// Check the input send a message as an error or store the variables' value
+		printf("Usage: DoctorsOffice <waitingSize> <patients> <doctors>\n");
+		exit(0);
+	}
+	else {																
+		WRSize	=	AvailableChairs	=	atoi(argv[1]);					// Size of waiting room and available chairs are the same at the begining as there is no Patient on the queue
+		NumPatient	=atoi(argv[2]);										// Total patients that should be generated
+		NumDR	=atoi(argv[3]);											// The number of DRs that should be generated
+	}
+	if ((NumDR==0)||(NumPatient==0)){									// the cinic run is useless without DRs and or patients
+		printf("Error: There should be atleast one Doctor and one patient for the program to start \n");
+		exit(0);
+	}
+    printf("#doctors %d, #patients %d, #chairs %d \n", NumDR, NumPatient, WRSize);
+ 
+	/* Patient Queue initialization */
+	pthread_cond_init (&PQ.cond, NULL);									// initialize the condition variable to defaults
+	pthread_mutex_init (&PQ.mutex, NULL);								// Initialize the mutex to defaults
+	PQ.queue_head	=0;													// set queu head to zero as no ticket is called at the begining
+	PQ.queue_tail	=0;													// set queue tail to zero az no ticket is generated
+ 
+	/* threads of DRs and Patients*/ 
+	pthread_t	Doctor	[NumDR];		
+	pthread_t	Patient	[NumPatient];
+ 
+	// Generate Doctor threads
+	pthread_mutex_lock	(&DRsOffice);									// Lock the mutex until all DRs are generated
+	int	count	=0;
+	for (count=0; count < NumDR; ++count){								// create threads in a loop
+		pthread_create ( &Doctor[count], NULL, doctor_thread, NULL);	// create Doctor threads with specific ID and the function doctor_thread
+		printf ("@user: Doctor Thread #%d is created", count);
+	}
+	pthread_mutex_unlock (&DRsOffice);									// unlock the mutex and let DR threads to proceed
+ 
+	// Generate Patient Threads
+	for (count=0; count < NumPatient; ++count){							// create threads in a loop
+		pthread_create ( &Patient[count], NULL, patient_thread, NULL);	// create Patient threads with specific ID and the function patient_thread
+		printf ("@user: Patient Thread #%d is created", count);
+		int range = MAX_SLEEP-MIN_SLEEP+1;								// set the range of the random sleep time
+		long W_time;
+		W_time= random();												// generate a random number
+		unsigned int Time=MIN_SLEEP + W_time % range;					// generate a random number between 1-5 seconds
+		printf ("@user: Sleep time #%d is", count);
+		sleep (Time);													// sleep the main thread for the random period
+	}
+	pthread_mutex_lock (&DRsOffice);										// lock the mutex to change ReceptionDone flag
+	ReceptionDone=1;													// change the flag to indicate that all patient threads are created
+	pthread_mutex_unlock (&DRsOffice);									// unlock mutex so other threads can lock it
+ 
+	//TODO: Clean up
+	for (count=0; count<NumPatient; ++count){							// Join all patient threads upon their exits
+		pthread_join (Patient[count], NULL);
+	}
+	for (count=0; count<NumDR; ++count){								// Join all doctor threads upon their exits
+		pthread_join (Doctor[count], NULL);
+	}
+	pthread_mutex_destroy (&PQ.mutex);									// destroy the mutex
+	pthread_mutex_destroy (&DRsOffice);									// destroy the mutex
+	return 0;	
 }
-		
-		
-	
-
-
-
-void* patient1(void* pv)
-{
-	sem_wait(&s1);
-	pthread_mutex_lock(&m1);
-	char str[]= "Rakesh";
-	int IDn= 785;
-	printf("\nPatient name : %s \n Patient ID : %d", str,IDn);
-	CheckVitals(15,65);
-
-	
-	pthread_mutex_unlock(&m1);
-	sem_post(&s1);
-	sem_post(&s2);
-	
-
-	//pthread_exit(NULL);
-}
-
-void* patient2(void* pv)
-{
-	sem_wait(&s2);
-	pthread_mutex_lock(&m1);
-	char str[]= "Vijay";
-	int IDn= 788;
-	printf("\nPatient name : %s \n Patient ID : %d", str,IDn);
-	CheckVitals(10,110);
-
-	
-	pthread_mutex_unlock(&m1);
-	sem_post(&s2);
-	sem_post(&s3);
-
-	//pthread_exit(NULL);
-}
-
-void* patient3(void* pv)
-{
-	sem_wait(&s3);
-	pthread_mutex_lock(&m1);
-	char str[]= "Vikram";
-	int IDn= 790;
-	printf("\nPatient name : %s \n Patient ID : %d", str,IDn);
-	CheckVitals(25,50);
-
-	
-	pthread_mutex_unlock(&m1);
-	sem_post(&s3);
-	sem_post(&s4);
-	
-
-	//pthread_exit(NULL);
-}
-
-void* patient4(void* pv)
-{
-	sem_wait(&s4);
-	pthread_mutex_lock(&m1);
-	char str[]= "Ajay";
-	int IDn= 795;
-	printf("Patient name : %s \n Patient ID : %d", str,IDn);
-	CheckVitals(14,80);
-
-	
-	pthread_mutex_unlock(&m1);
-	sem_post(&s4);
-	
-
-	//pthread_exit(NULL);
-}
-
-
-
-
-int main()
-{
-	pthread_t pt1,pt2,pt3,pt4;	   //thread handle
-	sem_init(&s1,0,1);
-	sem_init(&s2,0,0);
-	sem_init(&s3,0,0);
-	sem_init(&s4,0,0);
-	pthread_create(&pt1,NULL,patient1,NULL);
-	
-	pthread_create(&pt2,NULL,patient2,NULL);
-	
-	pthread_create(&pt3,NULL,patient3,NULL);
-	
-	pthread_create(&pt4,NULL,patient4,NULL);
-	
-	pthread_join(pt1,NULL);
-	pthread_join(pt2,NULL);
-	pthread_join(pt3,NULL);
-	pthread_join(pt4,NULL);
-	sem_destroy(&s1);
-	sem_destroy(&s2);
-	sem_destroy(&s3);
-	sem_destroy(&s4);
-	pthread_mutex_destroy(&m1);
-	printf("Thank you for the visit\n");
-	
-	
-	return 0;	                 //exit(0);
-}
-
