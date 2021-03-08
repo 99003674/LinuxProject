a73e2207faee80dc33275fcb81be9c814c951f50
99003575 <78539650+99003575@users.noreply.github.com>
2021-03-08 09:39:07 +0530

    Create Patientdoctor.h

--- PatientDoctor/Patientdoctor.h
+++ PatientDoctor/Patientdoctor.h
@@ -1,1 +1,16 @@
+#ifndef __PATIENTDOCTOR_H__
+#define __PATIENTDOCTOR_H__
 
+#include <pthread.h> 
+#include <semaphore.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+void treat_patient(void);
+void *doctor_thread(void *arg);
+void *patient_thread(void *arg);
+
+
+#endif
